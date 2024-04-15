5732a460544a1e0a78cf956a8b2e379166908bc4
antonio-corbalan-navarro <corbalannavarroantonio@gmail.com>
2024-04-15 18:24:22 +0200

    resources middleware

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { shareable, getResource, deleteResource } from '../controllers/resource.controller';
+import { isAllowedResource } from '../middlewares/resources';
 
 const routerV1 = Router()
 
 routerV1
-    .put('/:type/:table/:resourceId', shareable)
-    .get('/:type/:table/:resourceId', getResource)
-    .delete('/:type/:table/:resourceId', deleteResource)
+    .put('/:type/:table/:resourceId', isAllowedResource, shareable)
+    .get('/:type/:table/:resourceId', isAllowedResource, getResource)
+    .delete('/:type/:table/:resourceId', isAllowedResource, deleteResource)
 
 export { routerV1 } 
